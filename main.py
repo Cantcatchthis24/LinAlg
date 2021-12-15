c896ffef5293d570b1e7d4a3ff971993c5244265
Jonah D'Monte <cantcatchthis24@gmail.com>
2021-12-15 15:05:47 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+dimX = int(input("horizontal dimension"))
+dimY = int(input("vertical dimension"))
+
+
+def
+
+countX = 1
+row = [0]
+
+while countX != dimX:
+    row.append(countX)
+    countX += 1
+
+countY = 1
+matrix = [row]
+while countY != dimY:
+    matrix.append(row)
+    countY += 1
+def matrix(x, y):
+
+
+print(matrix)
